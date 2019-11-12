c0d4a446590697faa6a0edfb47ac739be99ec7ef
Hooje <48217854+Hooje@users.noreply.github.com>
2019-11-12 13:04:26 +0800

    Update 981-.-Assignment-2-Letter-Frequencies.cpp

--- 981-.-Assignment-2-Letter-Frequencies.cpp
+++ 981-.-Assignment-2-Letter-Frequencies.cpp
@@ -1,54 +1,34 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 using namespace std;
+int main()
+{
+	string s;
+	int A[26]={0};
+	int B[26]={0};
+	cin>>s;
 
-int main(){
-    string s;
-    int str[26],strc[26],temp,num;
-    //bool flag = false;
+	for(int i=0; i<s.length(); i++)
+	{
+		A[s[i]-'A']++;
+		B[s[i]-'A']++;
+	}
+	
+	sort(A,A + 26);
 
-    for(int i=0;i<26;i++){
-        str[i] = 0;
-        strc[i] = 0;
-    }
+	for (int i = 25; A[i]>0; i--)
+	{
+		for (int j = 0; j < 26 ; ++j)
+		{
+			if (A[i]==B[j])
+			{
+				cout<<j+65<<" "<<A[i]<<endl;
+				B[j]=0;
+				break;
+			}
+		}
+	}
 
-    getline(cin, s);
 
-    for(int i=0;i<s.length();i++){
-        s[i]-='A';
-        str[s[i]]++;
-   }
-
-    for(int i=0;i<26;i++){
-        strc[i] = str[i];
-    }
-
-    for(int i=0;i<25;i++){
-        for(int j=0;j<25-i;j++){
-            if(strc[j]<strc[j+1]){
-                temp = strc[j];
-                strc[j] = strc[j+1];
-                strc[j+1] = temp;
-            }
-        }
-    }
-
-    for(int i=0;i<26;i++){
-        temp = strc[i];
-        if(temp == 0) break;
-        for(int j=0;j<26;j++){
-            if(str[j] == temp){
-                cout << j+65 << " " << str[j] << endl;
-                str[j] = 0;
-                /*
-                if(flag == false) cout << endl;
-                for(int k=0;k<26;k++){
-                    if(str[k] == 0){
-                        flag = true;
-                    }
-                }
-                */
-            }
-        }
-    }
-
+	return 0;
 }
